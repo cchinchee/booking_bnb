cf5c12126bfa948be738964046a7600b54cd5bb8
CCHINCHEE <CHONGCC90@GMAIL.COM>
2018-01-11 17:28:15 +0800

    added secure image url under facebook omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
  # In config/initializers/omniauth.rb
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'],  scope: "public_profile", info_fields: "gender, name, email, picture"
+    provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'],  scope: "public_profile", info_fields: "gender, name, email, picture", secure_image_url: true
   	provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: "profile, email", info_fields: "gender, name, email, picture"
   end
